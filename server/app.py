82682f9cf8ce7c719c17c60ef14d719a614c8827
Manuel Manriquez <manriquezmanny@gmail.com>
2024-08-08 20:20:54 -0700

    manualy downloaded nltk punkt data before pushing it to repo, main app will simply load it

--- server/app.py
+++ server/app.py
@@ -13,11 +13,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Setting NLTK data path
 nltk_data_path = "./nltk_data"
-os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
-
-# Nltk Download
-nltk.download('punkt', download_dir=nltk_data_path)
 
 # Define the SentimentCNN class
 class SentimentCNN(nn.Module):
